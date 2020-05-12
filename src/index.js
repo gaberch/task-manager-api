3300d0a93058d860e9e1b13e5b5c6b7530aa6309
Gabe Richard <gabe_rch@hotmail.com>
2020-05-12 18:30:20 -0400

    Added more user and task endpoint tests

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
-const express = require('express');
+const app = require('./app');
 
-require('./db/mongoose')
-const userRouter = require('./routers/user');
-const taskRouter = require('./routers/task');
-
-const app = express();
 const port = process.env.PORT;
-
-app.use(express.json());     // automatically parses incoming requests to a json object
-app.use(userRouter);
-app.use(taskRouter);
 
 app.listen(port, () => {
     console.log('Server is up on port: ' + port)
--- tests/user.test.js
+++ tests/user.test.js
@@ -1,4 +1,124 @@
 const request = require('supertest');
+
 const app = require('../src/app');
+const User = require('../src/models/user');
+const { userOneId, userOne, setupDatabase} = require('../tests/fixtures/db');
+
+beforeEach(setupDatabase);
 
 
+test('Should signup a new user', async () => {
+    const response = await request(app).post('/users').send({
+        name: 'Gabe Richard',
+        email: 'gabe@test.com',
+        password: 'Test123!'
+    }).expect(201);
+
+    // Assert that the database was changed correctly
+    const user = await User.findById(response.body.user._id);
+    expect(user).not.toBeNull();
+
+    // Assertions about the response
+    expect(response.body).toMatchObject({
+        user: {
+            name: 'Gabe Richard',
+            email: 'gabe@test.com'
+        },
+        token: user.tokens[0].token
+    });
+    expect(user.password).not.toBe('Test123!');
+
+});
+
+test('Should login existing user', async () => {
+    const response = await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: userOne.password
+    }).expect(200);
+
+    const user = await User.findById(response.body.user._id);
+    expect(response.body.token).toBe(user.tokens[1].token);
+});
+
+test('Should not login nonexistent user', async () => {
+    await request(app).post('/users/login').send({
+        email: 'non-existent@test.com',
+        password: 'Nonexistentuser123!'
+    }).expect(400);
+});
+
+test('Should get profile for user', async () => {
+    await request(app)
+        .get('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200);
+})
+
+test('Shold not get user for unauthorized user', async() => {
+    await request(app)
+        .get('/users/me')
+        .send()
+        .expect(401);
+});
+
+test('Should delete account for authorized user', async () => {
+    await request(app)
+        .delete('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200);
+    
+    const user = await User.findById(userOneId);
+    expect(user).toBeNull();
+});
+
+test('Should not delete account for unauthorized user', async () => {
+    await request(app)
+        .delete('/users/me')
+        .send()
+        .expect(401);
+})
+
+test('Should upload avatar image', async () => {
+    await request(app)
+        .post('/users/me/avatar')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .attach('avatar','tests/fixtures/profile-pic.jpg')
+        .expect(200);
+    
+        const user = await User.findById(userOneId);
+        expect(user.avatar).toEqual(expect.any(Buffer));
+});
+
+test('Should update valid user fields', async () => {
+    await request(app)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            name: 'Gabe'
+        })
+        .expect(200);
+
+    const user = await User.findById(userOneId);
+    expect(user.name).toBe('Gabe');
+});
+
+test('Should not update invalid user fields', async () => {
+    await request(app)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            location: 'invalid field'
+        })
+        .expect(400);
+});
+
+
+//
+// User Test Ideas
+//
+// Should not signup user with invalid name/email/password
+// Should not update user if unauthenticated
+// Should not update user with invalid name/email/password
+// Should not delete user if unauthenticated
